6dd735d01fbd429320f6e972deccc05530bbddd2
RasikaHari <rasikahari06@gmail.com>
2025-08-25 18:48:24 +0530

    controller-cors

--- springapp/src/main/java/com/examly/springapp/controller/ApiController.java
+++ springapp/src/main/java/com/examly/springapp/controller/ApiController.java
@@ -30,8 +30,9 @@ import com.examly.springapp.service.UserService;
 import com.examly.springapp.util.PdfGeneratorUtil;
 
 @RestController
-// @CrossOrigin(origins = "http://localhost:3000", allowCredentials = "true") 
-@CrossOrigin(origins = "https://certificate-request-portal.onrender.com", allowCredentials = "true") 
+@CrossOrigin(origins = {"http://localhost:3000", "https://certificate-request-portal.onrender.com"}, allowCredentials = "true")
+
+
 public class ApiController {
 
     private final CertificateService service;
