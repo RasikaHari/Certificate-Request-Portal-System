73039f047c7333dbc5ed786f5e58addc3308b867
RasikaHari <rasikahari06@gmail.com>
2025-08-25 18:06:44 +0530

    api-baseurl-change(frontend-backend-connect)

--- reactapp/src/services/api.jsx
+++ reactapp/src/services/api.jsx
@@ -2,7 +2,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080', 
+  // baseURL: 'http://localhost:8080', 
+  baseURL:'https://certificate-request-portal-system-e01i.onrender.com/',
    withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
